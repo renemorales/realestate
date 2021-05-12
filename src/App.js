3e0f6963e7830741350bb057815135ba3a6d9988
Rene Morales <renemorales.developer@gmail.com>
2021-05-11 21:11:16 -0500

    created some dummy data and imported to AppJS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import React, { Component } from 'react';
 import { Header } from './components/header/Header';
 import { Filter } from './components/filter/Filter';
 import { Listings } from './components/listings/Listings';
+import  ListingsData  from './components/data/ListingsData';  
 
 
 import './App.css';
 
 class App extends Component {
   constructor(){
-    super(); // access to this.state
+    super();
 
     this.state = {
-    };
+      ListingsData
+    }
   }
 
   render(){
+    console.log(this.state.ListingsData);
     return (
       <div className="App">
         <Header />
---
+++ src/components/data/ListingsData.js
@@ -0,0 +1,96 @@
+const ListingsData = [ 
+  {
+    address: '4236 Winding Brook Dr',
+    city: 'Plano',
+    state: 'TX',
+    bedrooms: 3,
+    price: 344900,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym',
+      'garage',
+      'swimming pool'
+    ],
+    homeType: [
+      'Apartment',
+      'Home',
+      'Ranch'
+    ],
+    image: 'https://assets.site-static.com/userFiles/1075/image/si-modern-home.png'
+  },
+  {
+    address: '3904 Stockton Ln',
+    city: 'Dallas',
+    state: 'TX',
+    bedrooms: 3,
+    price: 338900,
+    floorSpace: 2000,
+    extras: [
+      'garage',
+      'swimming pool'
+    ],
+    homeType: [
+      'Apartment',
+      'Home',
+      'Ranch'
+    ],
+    image: 'https://www.utahrealestate.com/site/img/common/marquee-08.jpg'
+  },
+  {
+    address: '928 Teakwood Dr',
+    city: 'Richardson',
+    state: 'TX',
+    bedrooms: 3,
+    price: 425000,
+    floorSpace: 2160,
+    extras: [
+      'gym',
+      'garage',
+      'swimming pool'
+    ],
+    homeType: [
+      'Home',
+      'Ranch'
+    ],
+    image: 'https://www.opendoor.com/w/wp-content/uploads/2019/06/cost-selling-house-houston.jpg'
+  },
+  {
+    address: '243 High Brook Dr',
+    city: 'Plano',
+    state: 'TX',
+    bedrooms: 3,
+    price: 474080,
+    floorSpace: 3329,
+    extras: [
+      'elevator',
+      'gym',
+      'garage',
+      'swimming pool'
+    ],
+    homeType: [
+      'Home',
+      'Ranch'
+    ],
+    image: 'https://propertyspark.com/wp-content/uploads/2017/06/4-Exterior-copy-2.jpg'
+  },
+   {
+    address: '1201 Plaza Way',
+    city: 'Richardson',
+    state: 'TX',
+    bedrooms: 6,
+    price: 555000,
+    floorSpace: 2533,
+    extras: [
+      'gym',
+      'garage',
+      'swimming pool'
+    ],
+    homeType: [
+      'Home',
+    ],
+    image: 'https://cdn.sitephotos.sierrastatic.com/1188_hero_tempforandrea-20190430013354.jpg'
+  }
+]
+
+export default ListingsData;
